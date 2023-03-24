93bc801ec7215dd50d5372388fec70055a8de295
omarube <olivermarube@gmail.com>
2023-03-24 14:56:15 -0700

    add 0x04-more_functions_nested_loops codes

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+s (11 sloc)  163 Bytes
+#include "../main.h"
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
